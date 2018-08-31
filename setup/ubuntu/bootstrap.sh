c66414898590aab624b9e0b53a68ccc02e068a06
root <root@working-instance.c.analyze-residential.internal>
2018-08-31 15:54:14 +0000

    setup ubuntu file update

--- setup/ubuntu/bootstrap.sh
+++ setup/ubuntu/bootstrap.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #
-# This script setups Redash along with supervisor, nginx, PostgreSQL and Redis. It was written to be used on 
+# This script setups Redash along with supervisor, nginx, PostgreSQL and Redis. It was written to be used on
 # Ubuntu 16.04. Technically it can work with other Ubuntu versions, but you might get non compatible versions
 # of PostgreSQL, Redis and maybe some other dependencies.
 #
-# This script is not idempotent and if it stops in the middle, you can't just run it again. You should either 
+# This script is not idempotent and if it stops in the middle, you can't just run it again. You should either
 # understand what parts of it to exclude or just start over on a new VM (assuming you're using a VM).
 
 set -eu
 
-REDASH_BASE_PATH=/opt/redash
+REDASH_BASE_PATH=$HOME/.redash
 REDASH_BRANCH="${REDASH_BRANCH:-master}" # Default branch/version to master if not specified in REDASH_BRANCH env var
-REDASH_VERSION=${REDASH_VERSION-4.0.1.b4038} # Install latest version if not specified in REDASH_VERSION env var
-LATEST_URL="https://s3.amazonaws.com/redash-releases/redash.${REDASH_VERSION}.tar.gz"
+REDASH_VERSION=${REDASH_VERSION-5.0.0-beta} # Install latest version if not specified in REDASH_VERSION env var
+LATEST_URL="https://github.com/getredash/redash/archive/v${REDASH_VERSION}.tar.gz"
 VERSION_DIR="$REDASH_BASE_PATH/redash.${REDASH_VERSION}"
 REDASH_TARBALL=/tmp/redash.tar.gz
 FILES_BASE_URL=https://raw.githubusercontent.com/getredash/redash/${REDASH_BRANCH}/setup/ubuntu/files
@@ -41,7 +41,7 @@ install_system_packages() {
     # Base packages
     apt install -y python-pip python-dev nginx curl build-essential pwgen
     # Data sources dependencies:
-    apt install -y libffi-dev libssl-dev libmysqlclient-dev libpq-dev freetds-dev libsasl2-dev
+    apt install -y libffi-dev libssl-dev default-libmysqlclient-dev libpq-dev freetds-dev libsasl2-dev
     # SAML dependency
     apt install -y xmlsec1
     # Storage servers
@@ -52,7 +52,7 @@ install_system_packages() {
 create_directories() {
     mkdir -p $REDASH_BASE_PATH
     chown redash $REDASH_BASE_PATH
-    
+
     # Default config file
     if [ ! -f "$REDASH_BASE_PATH/.env" ]; then
         sudo -u redash wget "$FILES_BASE_URL/env" -O $REDASH_BASE_PATH/.env
@@ -67,15 +67,15 @@ extract_redash_sources() {
     sudo -u redash mkdir "$VERSION_DIR"
     sudo -u redash tar -C "$VERSION_DIR" -xvf "$REDASH_TARBALL"
     ln -nfs "$VERSION_DIR" $REDASH_BASE_PATH/current
-    ln -nfs $REDASH_BASE_PATH/.env $REDASH_BASE_PATH/current/.env
+    ln -nfs $REDASH_BASE_PATH/.env $REDASH_BASE_PATH/current/redash-$REDASH_VERSION/.env
 }
 
 install_python_packages() {
-    pip install --upgrade pip==9.0.3
+    pip install --upgrade pip==18.0
     # TODO: venv?
     pip install setproctitle # setproctitle is used by Celery for "pretty" process titles
-    pip install -r $REDASH_BASE_PATH/current/requirements.txt
-    pip install -r $REDASH_BASE_PATH/current/requirements_all_ds.txt
+    pip install -r $REDASH_BASE_PATH/current/redash-$REDASH_VERSION/requirements.txt
+    pip install -r $REDASH_BASE_PATH/current/redash-$REDASH_VERSION/requirements_all_ds.txt
 }
 
 create_database() {
@@ -83,7 +83,7 @@ create_database() {
     sudo -u postgres createuser redash --no-superuser --no-createdb --no-createrole
     sudo -u postgres createdb redash --owner=redash
 
-    cd $REDASH_BASE_PATH/current
+    cd $REDASH_BASE_PATH/current/redash-$REDASH_VERSION/
     sudo -u redash bin/run ./manage.py database create_tables
 }
 
